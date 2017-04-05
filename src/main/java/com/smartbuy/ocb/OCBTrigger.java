4972ed2fa1495fa983da14d1208fea9920d5870b
RISHI <Rishi rishi.h2kinfosys@gmail.com>
2017-04-04 22:41:23 -0400

    Initial Commit for OCB

---
+++ src/main/java/com/smartbuy/ocb/OCBTrigger.java
@@ -0,0 +1,12 @@
+package com.smartbuy.ocb;
+
+/**
+ * OCB Trigger
+ *
+ */
+public class OCBTrigger {
+	
+    public static void main( String[] args ) {
+        System.out.println( "This is OCB Start Logic" );
+    }
+}
