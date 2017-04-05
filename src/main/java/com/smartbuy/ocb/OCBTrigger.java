2e9771dea614226c1e9efa4234ac1095be3796b6
RISHI <Rishi rishi.h2kinfosys@gmail.com>
2017-04-04 22:43:12 -0400

    Commiting test

--- src/main/java/com/smartbuy/ocb/OCBTrigger.java
+++ src/main/java/com/smartbuy/ocb/OCBTrigger.java
@@ -8,5 +8,6 @@ public class OCBTrigger {
 	
     public static void main( String[] args ) {
         System.out.println( "This is OCB Start Logic" );
+        System.out.println("I am adding this change for checking in Git");
     }
 }
