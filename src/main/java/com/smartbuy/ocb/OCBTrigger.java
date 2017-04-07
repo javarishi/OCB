fff0aa5ffa390ae7b45ccf6887e6b649fcce9d9f
RISHI <Rishi rishi.h2kinfosys@gmail.com>
2017-04-06 22:30:12 -0400

    Checking in = RISHI

--- src/main/java/com/smartbuy/ocb/OCBTrigger.java
+++ src/main/java/com/smartbuy/ocb/OCBTrigger.java
@@ -7,7 +7,8 @@ package com.smartbuy.ocb;
 public class OCBTrigger {
 	
     public static void main( String[] args ) {
-        System.out.println( "This is OCB Start Logic" );
-        System.out.println("I am adding this change for checking in Git");
+       //Here you should take storenumber as input 
+    	
+    	// Business Object
     }
 }
---
+++ src/main/java/com/smartbuy/ocb/dao/DBUtil.java
@@ -0,0 +1,16 @@
+package com.smartbuy.ocb.dao;
+
+import java.sql.Connection;
+
+public class DBUtil {
+	
+	
+	 public Connection getConnection(){
+		 return null;
+	 }
+	 
+	 public void closeConnection(Connection conn){
+		 
+	 }
+
+}
